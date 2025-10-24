d8e85c1f710bd6b719b8fe9d3e97d6bd2c2cd41f
Abdelwahab Anwr <77800091+aa366@users.noreply.github.com>
2025-10-24 20:45:41 +0300

    😁

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import netlifyPlugin from "@netlify/vite-plugin-react-router";
-import { reactRouter } from "@react-router/dev/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), netlifyPlugin(), reactRouter()],
+  plugins: [react()],
 });
